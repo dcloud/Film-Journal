7b43b70422f0ebc5c33e951f675609cbec5b5181
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-07-20 21:54:29 -0400

    Initial Commit

---
+++ Film Journal/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Film Journal
+//
+//  Created by Daniel Cloud on 7/20/14.
+//  Copyright (c) 2014 Daniel Cloud. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Film JournalTests/Film_JournalTests.swift
@@ -0,0 +1,36 @@
+//
+//  Film_JournalTests.swift
+//  Film JournalTests
+//
+//  Created by Daniel Cloud on 7/20/14.
+//  Copyright (c) 2014 Daniel Cloud. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Film_JournalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
